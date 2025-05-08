8999d810962e0bf3ec30dca89c70b1d2b0bbc164
Arkar Myint Myat <arkarmyintmyat71@gmail.com>
2025-05-08 17:08:25 +0700

    completed checklist lesson by using PyQt5
    # Conflicts:
    #	PyQt5/checklist/checklist.ui
    #	PyQt5/main.py

--- PyQt5/checklist/checklist.py
+++ PyQt5/checklist/checklist.py
@@ -6,23 +6,22 @@ import sys
 class Main(QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
-        loadUi("main.ui", self)
-        to_do_lists = ["First", "Second", "Third"]
+        loadUi("checklist.ui", self)
+        to_do_lists = ["First", "Second", "Third", "Fourth"]
         for todo in to_do_lists:
             item = QListWidgetItem(todo)
             item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
             item.setCheckState(Qt.Unchecked)
             self.todo_listWidget.addItem(item)
-            self.toggle_all.clicked.connect(self.toggle_all_button)
+        self.toggle_all_button.clicked.connect(self.toggle_all)
 
-    def toggle_all_button(self):
+    def toggle_all(self):
         for i in range(self.todo_listWidget.count()):
             item = self.todo_listWidget.item(i)
             if item.checkState() == Qt.Checked:
                 item.setCheckState(Qt.Unchecked)
             else:
                 item.setCheckState(Qt.Checked)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
