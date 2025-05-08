2cf681b0d5e41eb1d01103fddef19c7baef04c4e
Arkar Myint Myat <arkarmyintmyat71@gmail.com>
2025-05-08 14:31:01 +0700

    Bug fix(PyQt5.Widget and PyQt5.QtCore) in PyQt5 folder

--- PyQt5/main.py
+++ PyQt5/main.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidgetItem
+from PyQt5.QtCore import Qt
 from PyQt5.uic import loadUi
 import sys
 
@@ -6,6 +7,12 @@ class Main(QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
         loadUi("main.ui", self)
+        to_do_lists = ["First", "Second", "Third"]
+        for todo in to_do_lists:
+            item = QListWidgetItem(todo)
+            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
+            item.setCheckState(Qt.Unchecked)
+            self.todo_listWidget.addItem(item)
 
 
 if __name__ == '__main__':
